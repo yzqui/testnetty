ae4eec979b020b97427424e849c855600e3990e1
zhiqiang.yu <Yzq19950715>
2020-10-29 17:06:07 +0800

    update

--- src/main/java/com/yuzhq/handler/EchoServerHandler.java
+++ src/main/java/com/yuzhq/handler/EchoServerHandler.java
@@ -1,25 +1,37 @@
 package com.yuzhq.handler;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
 
 /**
  * 服务器逻辑
  */
+@ChannelHandler.Sharable // 标记这类的实例之间可以在 channel 里面共享
 public class EchoServerHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        super.channelRead(ctx, msg);
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("server received : " + in.toString(CharsetUtil.UTF_8));
+        // 将接受到的消息返回给发送者。这里还没有冲刷数据
+        ctx.write(in);
     }
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-        super.channelReadComplete(ctx);
+        // 冲刷所有待审数据到远程节点
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        super.exceptionCaught(ctx, cause);
+        // 打印异常堆栈跟踪
+        cause.printStackTrace();
+        ctx.close();
     }
 }
