f98060560fdc18b0b61bfb5fee37760cb31fc7dd
zhiqiang.yu <Yzq19950715>
2020-10-29 16:19:30 +0800

    Initial commit

---
+++ src/main/java/com/yuzhq/handler/EchoServerHandler.java
@@ -0,0 +1,25 @@
+package com.yuzhq.handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * 服务器逻辑
+ */
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        super.channelRead(ctx, msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        super.channelReadComplete(ctx);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+    }
+}
