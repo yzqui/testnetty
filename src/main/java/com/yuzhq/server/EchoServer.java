be093283ff1b9aba73b09fdcbf3a4c861c103a88
zhiqiang.yu <Yzq19950715>
2020-10-30 10:57:41 +0800

    update

--- src/main/java/com/yuzhq/server/EchoServer.java
+++ src/main/java/com/yuzhq/server/EchoServer.java
@@ -10,12 +10,6 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 import java.net.InetSocketAddress;
 
-/**
- * 引导服务器
- * <p>
- * 监听和接收进来的连接请求
- * 配置 Channel 来通知一个关于入站消息的 EchoServerHandler 实例
- */
 public class EchoServer {
 
     private final int port;
@@ -27,12 +21,12 @@ public class EchoServer {
 
     public static void main(String[] args) throws Exception {
         if (args.length != 1) {
-            System.out.println("usage:" + EchoServer.class.getSimpleName() + "<port>");
+            System.err.println("usage:" + EchoServer.class.getSimpleName() + "<port>");
             return;
         }
-        int port = Integer.parseInt(args[0]);    // 设置端口值
-        System.out.println("port:::" + port);
-        new EchoServer(port).start();            // 呼叫服务器的 start 方法
+        int port = Integer.parseInt(args[0]);
+        System.err.println("111111111111111111111111111111111111port:::" + port);
+        new EchoServer(port).start();
     }
 
     public void start() throws Exception {
@@ -40,20 +34,20 @@ public class EchoServer {
         try {
             ServerBootstrap b = new ServerBootstrap();
             b.group(group)
-                    .channel(NioServerSocketChannel.class)   // 指定使用 NIO 的传输 channel
-                    .localAddress(new InetSocketAddress(port)) // 设置 socket 地址使用所选的端口
-                    .childHandler(new ChannelInitializer<SocketChannel>() { // 添加 EchoServerHandler 到 Channel 的 ChannelPipeline
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
                             ch.pipeline().addLast(new EchoServerHandler());
                         }
                     });
 
-            ChannelFuture f = b.bind().sync();      // 绑定的束武器，sync 等待服务器关闭
+            ChannelFuture f = b.bind().sync();
             System.out.println(EchoServer.class.getName() + " started and listen on " + f.channel().localAddress());
             f.channel().closeFuture().sync();
         } finally {
-            group.shutdownGracefully().sync();  // 释放所有资源
+            group.shutdownGracefully().sync();
         }
 
     }
